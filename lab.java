30a33c2e4cf8a9358f5a091f303161a2da9c7824
praveen-chowhan-99 <praveenkorra.office@gmail.com>
2025-04-22 09:30:45 +0530

    lab.java is changed

--- lab.java
+++ lab.java
@@ -1,5 +1,6 @@
 public class lab {
     public static void main(String[] args) {
         System.out.println("Hello, World! lab");
+        System.out.println("Hello, World! lab changed");
     }
 }
